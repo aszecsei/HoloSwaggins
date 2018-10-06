a43a28a7a0c1fc08c0663128dc40b29f9e615327
Alic Szecsei <aszecsei@gmail.com>
2018-10-06 15:05:54 -0500

    Update code style

--- Assets/Scripts/PhotoTaker.cs
+++ Assets/Scripts/PhotoTaker.cs
@@ -4,101 +4,104 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine.Serialization;
 using UnityEngine.XR.WSA.WebCam;
 
 public class PhotoTaker : MonoBehaviour
 {
-	public Shader shader;
-	public string screenShotURL = "http://www.argon-key-218614.appspot.com/transcribe";
+	[FormerlySerializedAs("shader")] public Shader Shader;
+	[FormerlySerializedAs("screenShotURL")] public string ScreenShotUrl = "http://www.argon-key-218614.appspot.com/transcribe";
 	
-	private PhotoCapture photoCapture = null;
-	private Texture2D targetTexture = null;
-	private Resolution cameraResolution;
+	private PhotoCapture _photoCapture;
+	private Texture2D _targetTexture;
+	private Resolution _cameraResolution;
 	
 	// Use this for initialization
-	void Start ()
+	private void Start ()
 	{
-		cameraResolution =
+		_cameraResolution =
 			PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
-		targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+		_targetTexture = new Texture2D(_cameraResolution.width, _cameraResolution.height);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	private void Update () {
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			TakePhoto();
 		}
 	}
 
-	void TakePhoto()
+	private void TakePhoto()
 	{
 		// Create a PhotoCapture object
 		PhotoCapture.CreateAsync(false, delegate(PhotoCapture captureObject)
 		{
-			photoCapture = captureObject;
-			CameraParameters cameraParameters = new CameraParameters();
-			cameraParameters.hologramOpacity = 0.0f;
-			cameraParameters.cameraResolutionWidth = cameraResolution.width;
-			cameraParameters.cameraResolutionHeight = cameraResolution.height;
-			cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
-			
+			_photoCapture = captureObject;
+			var cameraParameters = new CameraParameters
+			{
+				hologramOpacity = 0.0f,
+				cameraResolutionWidth = _cameraResolution.width,
+				cameraResolutionHeight = _cameraResolution.height,
+				pixelFormat = CapturePixelFormat.BGRA32
+			};
+
 			// Activate the camera
-			photoCapture.StartPhotoModeAsync(cameraParameters, delegate(PhotoCapture.PhotoCaptureResult result)
+			_photoCapture.StartPhotoModeAsync(cameraParameters, delegate(PhotoCapture.PhotoCaptureResult result)
 			{
 				// Take a picture
-				photoCapture.TakePhotoAsync(OnCapturedPhotoToMemory);
+				_photoCapture.TakePhotoAsync(OnCapturedPhotoToMemory);
 			});
 		});
 	}
 
-	void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+	private void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
 	{
 		// Copy the raw image data into the target texture
-		photoCaptureFrame.UploadImageDataToTexture(targetTexture);
+		photoCaptureFrame.UploadImageDataToTexture(_targetTexture);
 		
 		// Create a GameObject to which the texture can be applied
-		GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-		Renderer quadRenderer = quad.GetComponent<Renderer>();
-		quadRenderer.material = new Material(shader);
+		var quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+		var quadRenderer = quad.GetComponent<Renderer>();
+		quadRenderer.material = new Material(Shader);
 
 		quad.transform.parent = this.transform;
 		quad.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
 		
-		quadRenderer.material.SetTexture("_MainTex", targetTexture);
+		quadRenderer.material.SetTexture("_MainTex", _targetTexture);
 		
 		// Upload the photo
 		StartCoroutine(UploadPNG());
 		
 		// Deactivate the camera
-		photoCapture.StopPhotoModeAsync(OnStoppedPhotoMode);
+		_photoCapture.StopPhotoModeAsync(OnStoppedPhotoMode);
 	}
 
-	void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+	private void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
 	{
 		// Shutdown the photo capture resource
-		photoCapture.Dispose();
-		photoCapture = null;
+		_photoCapture.Dispose();
+		_photoCapture = null;
 	}
 	
-	IEnumerator UploadPNG()
+	private IEnumerator UploadPNG()
 	{
 		// We should only read the screen after all rendering is complete
 		yield return new WaitForEndOfFrame();
 
 		// Encode texture into PNG
-		byte[] bytes = targetTexture.EncodeToPNG();
+		byte[] bytes = _targetTexture.EncodeToPNG();
 		string image64Array = Convert.ToBase64String(bytes);
 		// string bodyJsonString = "{\"image\": \"" + image64Array + "\"}";
 		// Debug.Log("Body JSON: " + bodyJsonString);
 
 		// Create a web request
-		List<IMultipartFormSection> formData = new List<IMultipartFormSection>
+		var formData = new List<IMultipartFormSection>
 		{
 			new MultipartFormDataSection("image=" + image64Array)
 		};
 		
-		UnityWebRequest request = UnityWebRequest.Post(screenShotURL, formData);
+		var request = UnityWebRequest.Post(ScreenShotUrl, formData);
 		request.chunkedTransfer = false;
 		
 		yield return request.SendWebRequest();
