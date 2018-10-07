95c0b51f64fe6b57fde32f4d380f69e7bef2a6dc
Alic Szecsei <aszecsei@gmail.com>
2018-10-07 08:54:52 -0500

    Add menu

---
+++ Assets/Scripts/LanguageSelector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using TMPro;
+using UnityEngine;
+
+public class LanguageSelector : MonoBehaviour, IFocusable, IInputClickHandler
+{
+
+	public string Language;
+	public PhotoTaker PhotoTaker;
+	private TextMeshPro _text;
+
+	void Awake()
+	{
+		_text = transform.GetComponentInChildren<TextMeshPro>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnFocusEnter()
+	{
+		_text.color = Color.yellow;
+	}
+
+	public void OnFocusExit()
+	{
+		_text.color = Color.white;
+	}
+
+	public void OnInputClicked(InputClickedEventData eventData)
+	{
+		PhotoTaker.lang = Language;
+	}
+}
